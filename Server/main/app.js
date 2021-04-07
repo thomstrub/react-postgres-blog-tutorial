57f7302b67573e6cb99905c3e63f4f92d8647ce3
Thomas Strub <thomstrub@gmail.com>
2021-04-07 14:13:47 -0700

    fixes bug by changing port to 6000

--- Server/main/app.js
+++ Server/main/app.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 console.log('app.js firing');
+
 app.use('/', indexRouter)
 
 module.exports = app;
