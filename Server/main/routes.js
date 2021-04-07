91ea1396a8d4b582debc2fb7f4adb4f8fc178ab2
Thomas Strub <thomstrub@gmail.com>
2021-04-07 16:14:38 -0700

    adds put request connection in routes

--- Server/main/routes.js
+++ Server/main/routes.js
@@ -27,5 +27,31 @@ router.get('/api/get/allposts', (req, res, next ) => {
                 })
   })
 
+  router.post('/api/post/posttodb', (req, res, next) => {
+      const values = [req.body.title,
+                      req.body.body,
+                      req.body.uid,
+                      req.body.username]
+      pool.query(`INSERT INTO posts(title, body, user_id, author, date_created)
+                  VALUES($1, $2, $3, $4, NOW())`,
+                values, (q_err, q_res) => {
+                if(q_err) return next(q_err);
+                res.json(q_res.rows)
+                })
+  })
+ 
+  router.put('/api/put/post', (req, res, next) => {
+      const values = [req.body.title,
+                      req.body.body,
+                      req.body.uid,
+                      req.body.pid,
+                      req.body.username]
+      pool.query(`UPDATE posts SET title= $1, body=$2, user_id=$3, author=$5, date_created=NOW()
+        WHERE pid = $4`, values,
+        (q_err, q_res) => {
+            console.log(q_res)
+            console.log(q_err)
+        })
+  })
 
 module.exports = router
