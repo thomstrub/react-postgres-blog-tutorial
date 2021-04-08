bbf43dfbd5a43e6e555e8180d4e0d7a21bc3534e
Thomas Strub <thomstrub@gmail.com>
2021-04-07 20:01:48 -0700

    adds posts routes for db

--- Server/main/routes.js
+++ Server/main/routes.js
@@ -5,7 +5,7 @@ var pool = require('./db')
 router.get('/api/hello', (req, res) => {
     res.json('hello world')
     console.log('response sent')
-})
+});
 
 // POSTS ROUTES SECTION
 
@@ -14,8 +14,8 @@ router.get('/api/get/allposts', (req, res, next ) => {
                 ORDER BY date_created DESC`, 
               (q_err, q_res) => {
                     res.json(q_res.rows)
-    })
-  })
+    });
+  });
 
   router.get('/api/get/post', (req, res, next) => {
       const post_id = req.query.post_id
@@ -24,8 +24,8 @@ router.get('/api/get/allposts', (req, res, next ) => {
                 WHERE pid=$1`,
                 [post_id], (q_err, q_res) => {
                     res.json(q_res.rows)
-                })
-  })
+                });
+  });
 
   router.post('/api/post/posttodb', (req, res, next) => {
       const values = [req.body.title,
@@ -37,8 +37,8 @@ router.get('/api/get/allposts', (req, res, next ) => {
                 values, (q_err, q_res) => {
                 if(q_err) return next(q_err);
                 res.json(q_res.rows)
-                })
-  })
+                });
+  });
  
   router.put('/api/put/post', (req, res, next) => {
       const values = [req.body.title,
@@ -51,7 +51,46 @@ router.get('/api/get/allposts', (req, res, next ) => {
         (q_err, q_res) => {
             console.log(q_res)
             console.log(q_err)
-        })
-  })
+        });
+  });
+
+  router.delete('/api/delete/post', (req, res, next) => {
+      const post_id = req.body.post_id
+      pool.query(`DELETE FROM posts WHERE pid = $1`, [post_id],
+                (q_err, q_res) => {
+                    res.json(q_res.rows)
+                    console.log(q_err)
+                });
+  });
+
+  router.delete('/api/delete/postcomments', (req,res, next) => {
+      const post_id = req.body.post_id
+      pool.query(`DELETE FROM comments WHERE post_id = $1` [post_id],
+                  (q_err, q_res) => {
+                      res.json(q_res.rows)
+                      console.log(q_err)
+                  });
+  });
+
+  router.put('/api/put/likes', (req, res, next) => {
+      const uid = [req.body.uid]
+      const post_id = String(req.body.post_id)
+
+      const values = [uid, post_id]
+      console.log(values)
+      pool.query(`UPDATE posts
+                  SET like_user_id = like_user_id || $1, likes = likes +1
+                  WHERE NOT (like_user_id @> $1)
+                  AND pid = ($2)`,
+                values, (q_err, q_res) => {
+                    if(q_err) return next(q_err);
+                    console.log(q_res)
+                    res.json(q_res.rows);
+                });
+  });
+
+  // comments routes
+
+  router.post('/api/post/commenttodb', ())
 
 module.exports = router
