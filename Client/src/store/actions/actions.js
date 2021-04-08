db134f1192c3a01d321069eb024459c70a5f5659
Thomas Strub <thomstrub@gmail.com>
2021-04-08 14:09:56 -0700

    adds actions and action types

--- Client/src/store/actions/actions.js
+++ Client/src/store/actions/actions.js
@@ -1,3 +1,4 @@
+import Profile from '../../hooks/profile'
 import * as ACTION_TYPES from './action_types'
 
 export const SUCCESS = {
@@ -60,5 +61,31 @@ export const user_input_submit = (text) => {
   return {
     type: ACTION_TYPES.USER_INPUT_SUBMIT,
     payload: text
+  }
+}
+
+export const set_db_profile = (profile) => {
+  return {
+    type: ACTION_TYPES.SET_DB_PROFILE,
+    payload: profile
+  }
+}
+
+export const remove_db_profile = () => {
+  return {
+    type: ACTION_TYPES.REMOVE_DB_PROFILE
+  }
+}
+
+export const set_db_posts = (posts) => {
+  return {
+    type: ACTION_TYPES.FETCH_DB_POSTS,
+    payload: posts
+  }
+}
+
+export const remove_db_posts = () => {
+  return{
+    type: ACTION_TYPES.REMOVE_DB_POSTS
   }
 }
