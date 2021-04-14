b267358c7beb2e731fccaf046f6667c5e19f7f97
Thomas Strub <thomstrub@gmail.com>
2021-04-13 21:51:09 -0700

    adds RenderPosts to Profile.js

--- Client/src/components/Profile/profile.js
+++ Client/src/components/Profile/profile.js
@@ -47,4 +47,25 @@ const Profile = () => {
             .then(() => handleClickClose())
             .then(() => setTimeout(() => history.replace('/'), 700) )
     }
+
+    const RenderProfile = (props) => {
+        return(
+            <div>
+                <h1>{props.profile.profile.nickname}</h1>
+                <br />
+                <img src={props.profile.profile.picture} alt="" />
+                <br />
+                <h4> {props.profile.profile.email} </h4>
+                <br />
+                <h5> { props.profile.profile.name } </h5>
+                <br />
+                <h6> Email Verified: </h6>
+                { props.profile.profile.email_verified ? <p>Yes</p> : <p>No</p> }
+                <br />
+            </div>
+        )
+    }
+
+    
+
 }
